af1d9f1fc7aea4684631e13cd50c122cc1543066
André Abadesso <andre.abadesso@gmail.com>
2013-11-28 00:46:17 -0200

    Pro server sinistro

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var page = new WebPage();
 
-var i = 0;
 page.onCallback = function(data) {
 
 	if (data === "finished") {    
@@ -18,10 +17,8 @@ page.onCallback = function(data) {
 	
 	// Ignorando a primeira imagem:
 	if (data !== 0) { 
-		//page.render("./imagens/" + (data < 10 ? '0' + data : data)+ '.jpeg');
-		page.render("./imagens/" + i + ".jpeg");
-		console.log("Salvei Imagem");  
-		i++;
+		page.render("./imagens/" + (data < 10 ? '0' + data : data)+ '.jpeg');
+		console.log("Salvei Imagem");
 	}
 
 }
